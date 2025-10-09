7fd2074b482052e8fde23bbb892b65499524c262
Miriam K. Wolff <miriamkwolff@hotmail.com>
2025-10-09 11:03:29 +0200

    Updated required dependencies in setup

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup
 
 setup(
     name="simglucose",
-    version="0.2.11",
+    version="0.2.12",
     description="A Type-1 Diabetes Simulator as a Reinforcement Learning Environment in OpenAI gym or rllab (python implementation of UVa/Padova Simulator)",
-    url="https://github.com/jxx123/simglucose",
+    url="https://github.com/replicahealth/simglucose",
     author="Jinyu Xie",
     author_email="xjygr08@gmail.com",
     license="MIT",
     packages=["simglucose"],
     install_requires=[
-        "gym==0.9.4",
+        "gym>=0.26.0",
         "gymnasium~=0.29.1",
         "pathos>=0.3.1",
         "scipy>=1.11.0",
